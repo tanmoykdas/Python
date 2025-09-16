ec2a426fdcf6f990cfafdbb3586b2833a0745602
tanmoykdas <tnmy468@gmail.com>
2025-09-16 22:34:52 +0600

    iteration method

---
+++ Numerical Method/iterative method.py
@@ -0,0 +1,14 @@
+def iteration(g, x0, tol=12e-6, max_iter=100):
+    for i in range(max_iter):
+        x1 = g(x0)
+        print(f"Iteration {i+1}: x = {x1:.6f}")
+        if abs(x1 - x0) < tol:
+            print(f"Root is {x1:.6f} after {i+1} iterations.")
+            return x1
+        x0 = x1
+    print("Did not converge within the maximum number of iterations.")
+    return None
+
+g = lambda x: (1 - x)**(1/3) 
+x0 = 0.5
+iteration(g, x0)
