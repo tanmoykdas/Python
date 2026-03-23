7f9796ea09fab35827288145e547fb93a22c2ed3
tanmoykdas <tnmy468@gmail.com>
2026-03-23 14:06:42 +0600

    module 2 sesh korlam basic python er

---
+++ Practice/array.py
@@ -0,0 +1,6 @@
+n = 5
+a = [0] * n
+for i in range(n): 
+    a[i] = int(input())
+for i in range(n):
+    print(a[i])
---
+++ Practice/digitsum.py
@@ -0,0 +1,7 @@
+a = input()
+b = input()
+a = int(a)
+b = int(b)
+a = a % 10
+b = b % 10
+print (a + b)
